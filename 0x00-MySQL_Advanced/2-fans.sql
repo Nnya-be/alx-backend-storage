7fc5d93cfc9f4ab8df2f691efc79a6e6d952556b
Cha_mamme <solomonchamamme@gmail.com>
2024-10-08 21:52:58 +0000

    Created: the ranking of origins in the database

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,12 @@
--- A script to rank the origins of bancs based on the total number of non unique fans
+-- Ranking country origins of bands based on the total number of non-unique fans
+-- The table is assumed to be metal_bands, adjust it based on your actual table name
 
-SELECT COALESCE(origin, 'Unknown') AS origin,
-       COALESCE(SUM(fans), 0) AS total_fans  FROM metal_bands
-WHERE fans IS NOT NULL
-GROUP BY origin ORDER BY total_fans DESC;
+SELECT 
+    origin, 
+    SUM(fans) AS nb_fans
+FROM 
+    metal_bands
+GROUP BY 
+    origin
+ORDER BY 
+    nb_fans DESC;
