769c7087828e50fd5718c671fc8bffe94250e2a5
Cha_mamme <solomonchamamme@gmail.com>
2024-10-08 21:44:57 +0000

    Created: the ranking of origins in the database

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- A script to rank the origins of bancs based on the total number of non unique fans
 
 SELECT origin, SUM(fans) AS total_fans FROM metal_bands
-GROUP BY origin ORDER BY total_fans DESC;
+GROUP BY origin ORDER BY total_fans;
